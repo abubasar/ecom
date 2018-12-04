c524d97263f7e6b5555ab052200847423d742814
arif islam <arif.sec@outlook.com>
2018-12-04 14:27:42 +0600

    added category and product entity and added database

---
+++ clothbazar.database/CBContext.cs
@@ -0,0 +1,21 @@
+﻿using clothbazar.entities;
+using System;
+using System.Collections.Generic;
+using System.Data.Entity;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace clothbazar.database
+{
+   public class CBContext:DbContext
+    {
+        public CBContext():base("Default")
+        {
+
+        }
+        public DbSet<Product> Products { get; set; }
+        public DbSet<Category> Categories { get; set; }
+
+    }
+}
